c51646289901c944ad2059cc6c3ef13532ba1276
Nick Bennett <nick@nickbenn.com>
2020-12-08 03:44:40 -0700

    Prep for day 9.

---
+++ src/com/nickbenn/advent/day9/Main.java
@@ -0,0 +1,40 @@
+/*
+ *  Copyright 2020 Nicholas Bennett.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.nickbenn.advent.day9;
+
+import com.nickbenn.advent.util.Defaults;
+import com.nickbenn.advent.util.Parser;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.stream.Stream;
+
+public class Main {
+
+  public Main(String filename) throws URISyntaxException, IOException {
+    try (
+        Stream<String> stream = new Parser.Builder(getClass().getResource(filename).toURI())
+            .build()
+            .lineStream()
+    ) {
+
+    }
+  }
+
+  public static void main(String[] args) throws IOException, URISyntaxException {
+    Main main = new Main(Defaults.FILENAME);
+  }
+
+}
